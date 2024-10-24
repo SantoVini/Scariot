c7b31d57dbe657c43947086dd0a48ebdc6384faa
SantoVini <191508@upf.br>
2024-10-23 23:51:49 -0300

    Update Contato.blade.php

--- resources/views/Contato.blade.php
+++ resources/views/Contato.blade.php
@@ -1,10 +1,15 @@
 <x-app-layout>
     
 <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <img class="d-block mx-auto mb-4" src="https://www.google.com/imgres?q=scariot%20lanches&imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D100054649591322&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Fp%2FScariot-Lanches-100054649591322%2F&docid=e4UQW-hI4G5i-M&tbnid=9sIQI3TqAnKReM&vet=12ahUKEwjX6c3s_aWJAxVyr5UCHTW9DhIQM3oECBoQAA..i&w=360&h=360&hcb=2&ved=2ahUKEwjX6c3s_aWJAxVyr5UCHTW9DhIQM3oECBoQAA" alt="" width="72" height="57">
     <h1 class="display-5 fw-bold text-body-emphasis">Scariot</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <p class="lead mb-4">Scariot Lanches
+                            Restaurante
+                            Melhor Xis da região há mais de 5 anos;
+                            (54) 3342-5792
+                            (54) 99715-5499
+                            Terça - Sábado: 11:00-13:00 </p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
         <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
         <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
