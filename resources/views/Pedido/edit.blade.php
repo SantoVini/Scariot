8a4196d699fd94be120df91030b137c74ec4a1c6
SantoVini <191508@upf.br>
2024-11-24 18:20:45 -0300

    Update edit.blade.php

--- resources/views/Pedido/edit.blade.php
+++ resources/views/Pedido/edit.blade.php
@@ -1,23 +1,27 @@
 <x-app-layout>
     <section class="edit-form">
         <div class="form-container">
-            <form method="POST" action="{{ route('Produto.update', $Produto->id) }}">
+            <form method="POST" action="{{ route('Pedido.update', $Pedido->id) }}">
                 @csrf
                 @method('PUT')
                 
                 <div class="form-group">
-                    <label class="form-label" for="nome">Nome:</label>
-                    <input type="text" id="nome" name="nome" value="{{old('nome', $Produto->nome) }}" class="form-input">
+                    <label class="form-label" for="descrição">Descrição:</label>
+                    <input type="text" id="descrição" name="descrição" value="{{old('descrição', $Pedido->descrição) }}" class="form-input">
                 </div>
 
                 <div class="form-group">
-                    <label class="form-label" for="descricao">Descricao:</label>
-                    <input type="text" id="descricao" name="descricao" value="{{old('descricao', $Produto->descricao) }}" class="form-input">
+                    <label class="form-label" for="data_pedido">Data do Pedido:</label>
+                    <input type="text" id="data_pedido" name="data-pedido" value="{{old('data_pedido', $Pedido->data_pedido) }}" class="form-input">
                 </div>
 
                 <div class="form-group">
-                    <label class="form-label" for="valor_base">Valor base:</label>
-                    <input type="text" id="valor_base" name="valor_base" value="{{old('valor_base', $Produto->valor_base) }}" class="form-input">
+                    <label class="form-label" for="valor_total">Valor Total:</label>
+                    <input type="text" id="valor_total" name="valor_total" value="{{old('valor_total', $Pedido->valor_total) }}" class="form-input">
+                </div>
+                <div class="form-group">
+                    <label class="form-label" for="id_cliente">Id Cliente:</label>
+                    <input type="text" id="id_cliente" name="id_cliente" value="{{old('id_cliente', $Pedido->id_cliente) }}" class="form-input">
                 </div>
 
                 <div class="form-action">
