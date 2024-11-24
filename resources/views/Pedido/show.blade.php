f7fe5c2b8329ba4afcb7f9b4d2112b57c378c5d1
SantoVini <191508@upf.br>
2024-11-24 18:30:30 -0300

    Update show.blade.php

--- resources/views/Pedido/show.blade.php
+++ resources/views/Pedido/show.blade.php
@@ -1,28 +1,32 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-x1 text-gray-800 dark:text-gray-200 leading-tight">
-            {{__('Clientes') }}
+            {{__('Pedido') }}
         </h2>
     </x-slot>
     <section class="details">
         <div class="content">
             <div class="meta">
                 <span class="label">ID:</span>
-                <span class="info">{{ $Produto->id }}</span>
-            </div>
-            <div class="meta">
-                <span class="label">Nome:</span>
-                <span class="info">{{ $Produto->nome }}</span>
+                <span class="info">{{ $Pedido->id }}</span>
             </div>
             <div class="meta">
                 <span class="label">Descrição:</span>
-                <span class="info">{{ $Produto->descricao }}</span>
+                <span class="info">{{ $Pedido->descrição }}</span>
             </div>
             <div class="meta">
-                <span class="label">Valor Base:</span>
-                <span class="info">{{ $Produto->valor_base }}</span>
+                <span class="label">Data do Pedido:</span>
+                <span class="info">{{ $Pedido->data_pedido }}</span>
+            </div>
+            <div class="meta">
+                <span class="label">Valor Total:</span>
+                <span class="info">{{ $Pedido->valor_total }}</span>
+            </div>
+            <div class="meta">
+                <span class="label">ID Cliente:</span>
+                <span class="info">{{ $Pedido->id_cliente }}</span>
             </div>
         </div>
-        <a href="{{ route('Produto.index') }}" class="btn-return">Voltar</a>
+        <a href="{{ route('Pedido.index') }}" class="btn-return">Voltar</a>
     </section>
 </x-app-layout>
