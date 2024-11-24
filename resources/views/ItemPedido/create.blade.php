f09a262b53c3dc28b4c12d46edfa7f113223dd33
SantoVini <191508@upf.br>
2024-11-24 20:10:28 -0300

    Update create.blade.php

--- resources/views/ItemPedido/create.blade.php
+++ resources/views/ItemPedido/create.blade.php
@@ -1,22 +1,26 @@
 <x-app-layout>
     <div class="container">
-        <form action="{{ route('Produto.store') }}" method="POST">
+        <form action="{{ route('ItemPedido.store') }}" method="POST">
             @csrf
             <div class="input-group mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-default">Nome:</span>
-                <input type="text" name="nome" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Insira Testo">
+                <span class="input-group-text" id="inputGroup-sizing-default">Quantidade:</span>
+                <input type="text" name="quantidade" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Insira Testo">
             </div>
             <div class="input-group mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-default">Descrição:</span>
-                <input type="text" name="descricao" nameclass="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Username">
+                <span class="input-group-text" id="inputGroup-sizing-default">Valor:</span>
+                <input type="text" name="valor" nameclass="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Username">
             </div>
             <div class="input-group mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-default">Valor base:</span>
-                <input type="text" name="valor_base" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Username">
+                <span class="input-group-text" id="inputGroup-sizing-default">ID Pedido:</span>
+                <input type="text" name="id_pedido" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Username">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">ID Produto:</span>
+                <input type="text" name="id_produto" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Username">
             </div>
 
             <button type="submit" class="btn btn-sucess">Salvar</button>
-            <a href="{{ route('Produto.index')}}" class="btn btn-secondary">Cancelar</a>
+            <a href="{{ route('ItemPedido.index')}}" class="btn btn-secondary">Cancelar</a>
         </form>
     </div>    
 </x-app-layout>
