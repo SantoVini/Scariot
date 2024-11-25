6016a36f8f6ff2e4a3f24f5f087b4d3b07ac27a2
SantoVini <191508@upf.br>
2024-11-24 21:14:58 -0300

    Update edit.blade.php

--- resources/views/ItemPedido/edit.blade.php
+++ resources/views/ItemPedido/edit.blade.php
@@ -1,28 +1,33 @@
 <x-app-layout>
     <section class="edit-form">
         <div class="form-container">
-            <form method="POST" action="{{ route('Produto.update', $Produto->id) }}">
+            <form method="POST" action="{{ route('ItemPedido.update', $ItemPedido->id) }}">
                 @csrf
                 @method('PUT')
                 
                 <div class="form-group">
-                    <label class="form-label" for="nome">Nome:</label>
-                    <input type="text" id="nome" name="nome" value="{{old('nome', $Produto->nome) }}" class="form-input">
+                    <label class="form-label" for="quantidade">Quantidade:</label>
+                    <input type="text" id="quantidade" name="quantidade" value="{{old('quantidade', $ItemPedido->quantidade) }}" class="form-input">
                 </div>
 
                 <div class="form-group">
-                    <label class="form-label" for="descricao">Descricao:</label>
-                    <input type="text" id="descricao" name="descricao" value="{{old('descricao', $Produto->descricao) }}" class="form-input">
+                    <label class="form-label" for="valor">Valor:</label>
+                    <input type="text" id="valor" name="valor" value="{{old('valor', $ItemPedido->valor) }}" class="form-input">
                 </div>
 
                 <div class="form-group">
-                    <label class="form-label" for="valor_base">Valor base:</label>
-                    <input type="text" id="valor_base" name="valor_base" value="{{old('valor_base', $Produto->valor_base) }}" class="form-input">
+                    <label class="form-label" for="id_pedido">ID Pedido:</label>
+                    <input type="text" id="id_pedido" name="id_pedido" value="{{old('id_pedido', $ItemPedido->id_pedido) }}" class="form-input">
+                </div>
+
+                <div class="form-group">
+                    <label class="form-label" for="id_produto">ID Produto:</label>
+                    <input type="text" id="id_produto" name="id_produto" value="{{old('id_produto', $ItemPedido->id_produto) }}" class="form-input">
                 </div>
 
                 <div class="form-action">
                     <button type="submit" class="btn-primary">Salvar</button>
-                    <a href="{{ route('Cliente.index') }}" class="btn-secondary">Cancelar</a>
+                    <a href="{{ route('ItemPedido.index') }}" class="btn-secondary">Cancelar</a>
                 </div>
             </form>
         </div>
